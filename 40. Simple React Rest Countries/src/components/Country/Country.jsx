cd671f4a30ff192a210cdd08715ef85adc8de213
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-10-15 14:14:39 +0600

    40-3 Recap folder structure and Turn off prop types eslint warning

--- 40. Simple React Rest Countries/src/components/Country/Country.jsx
+++ 40. Simple React Rest Countries/src/components/Country/Country.jsx
@@ -5,20 +5,23 @@ export default function Country({ country }) {
     capital,
     population,
     area,
+    cca3,
   } = country || {};
 
-  console.log(country);
+  // https://restcountries.com/v3.1/alpha?codes=170
 
   return (
-    <div className="w-full shadow-xl h-80 card bg-base-100">
+    <div className="w-full h-[430px] shadow-xl card bg-base-100">
       <figure>
-        <img src={svg} alt={alt} className="w-full h-40" />
+        <img src={svg} alt={alt} className="w-full h-48" />
       </figure>
-      <div className="p-2 card-body">
+      <div className="p-4 card-body">
         <h2 className="text-xl font-medium card-title">Name: {common}</h2>
         <p>Capital: {capital}</p>
         <p>Population: {population}</p>
         <p>Area: {area} Sq. km</p>
+        <button className="btn btn-sm btn-neutral">Visited</button>
+        <button className="btn btn-sm btn-neutral">Code: {cca3}</button>
       </div>
     </div>
   );
