009fa3daac324420311788e89e1994ccc00d740b
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-11-11 10:00:35 +0600

    modify footer

--- 0. Conceptual Sessions/Batch-09/Mastering SPA Navigation/src/shared/Footer.tsx
+++ 0. Conceptual Sessions/Batch-09/Mastering SPA Navigation/src/shared/Footer.tsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="p-4 shadow-lg footer footer-center bg-base-200 text-base-content">
+      <aside>
+        <p>Copyright © 2024 - All right reserved by ByteBlaze</p>
+      </aside>
+    </footer>
+  );
 }
