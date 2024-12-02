bb696d0f9da927c9859c48e8fa5e4abe229416ac
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-12-02 09:34:03 +0600

    Refactor: Implement routes & controllers

---
+++ 55. Know More About Node.js, Express.js, MongoDB and CRUD operation/4. simple-crud-posts/backend-server/index.js
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { dbConnect, getDatabase } from './db.js';
+import postsRoutes from './routes/posts.route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//* Middleware
+app.use(cors());
+app.use(express.json());
+
+//* MongoDB Connection
+await dbConnect();
+
+//* Collections
+export const posts = getDatabase().collection('posts');
+
+//* Routes
+app.use('/posts', postsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
