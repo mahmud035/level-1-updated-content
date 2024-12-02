8e6a0cb03f587afa1210d1b69d098af1c880f063
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-12-02 18:35:26 +0600

    install morgan HTTP request logger middleware

--- 56. CRUD with Coffee Store/backend-server/index.js
+++ 56. CRUD with Coffee Store/backend-server/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 import { dbConnect, getDatabase } from './db.js';
 import coffeesRoutes from './routes/coffees.route.js';
 import usersRoutes from './routes/users.route.js';
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 //* Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 //* Initialize MongoDB
 await dbConnect();
