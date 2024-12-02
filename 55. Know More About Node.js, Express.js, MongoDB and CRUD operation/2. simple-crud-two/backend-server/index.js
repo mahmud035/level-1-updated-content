c29cc6114af23ed3dc1be8e2a87198efd065732e
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-12-02 13:01:03 +0600

    Refactor: Implement routes & controllers

--- 55. Know More About Node.js, Express.js, MongoDB and CRUD operation/2. simple-crud-two/backend-server/index.js
+++ 55. Know More About Node.js, Express.js, MongoDB and CRUD operation/2. simple-crud-two/backend-server/index.js
@@ -2,73 +2,26 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import { MongoClient, ObjectId } from 'mongodb';
+import { dbConnect, getDatabase } from './db.js';
+import usersRoutes from './routes/users.route.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-dotenv.config();
 
 //* Middleware
 app.use(cors());
 app.use(express.json());
 
-//* MongoDB Connection
-const uri = process.env.DATABASE_URI;
-const client = new MongoClient(uri);
-const dbName = process.env.DB_NAME;
-let database;
+//* Initialize MongoDB
+await dbConnect();
 
-const dbConnect = () => {
-  try {
-    database = client.db(dbName);
-    console.log(`Connected to MongoDB database: ${dbName}`);
-  } catch (error) {
-    console.log('Error connecting to MongoDB', error);
-  }
-};
-dbConnect();
+//* MongoDB Collections
+export const users = getDatabase().collection('users');
 
-//* Collections
-const users = database.collection('users');
-
-//* GET(READ)
-app.get('/users', async (req, res) => {
-  const query = {};
-  const result = await users.find(query).toArray();
-  res.send(result);
-});
-
-app.get('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const query = { _id: new ObjectId(id) };
-  const result = await users.findOne(query);
-  res.send(result);
-});
-
-//* POST(CREATE)
-app.post('/users', async (req, res) => {
-  const user = req.body;
-  const result = await users.insertOne(user);
-  res.send(result);
-});
-
-//* PUT/PATCH(UPDATE)
-app.put('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-  const filter = { _id: new ObjectId(id) };
-  const options = { upsert: true };
-  const result = await users.updateOne(filter, { $set: updatedUser }, options);
-  res.send(result);
-});
-
-//* DELETE
-app.delete('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const query = { _id: new ObjectId(id) };
-  const result = await users.deleteOne(query);
-  res.send(result);
-});
+//* Routes
+app.use('/users', usersRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
