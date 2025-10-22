fdec712a44a6c5042870c90e57cf1d0d5030f5e9
mahmud035 <mahamudulhasan4148@gmail.com>
2025-10-22 07:41:16 +0600

    Dockerized the project and push it into Docker Hub

--- 0-conceptual-sessions/batch-10/milestone-11/backend-with-jwt-and-axios-interceptor/client/src/config/axios.config.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, // Allow cookies to be sent
-  timeout: 10000,
-});
-
-export default axiosInstance;
--- 0-conceptual-sessions/batch-10/milestone-11/backend-with-jwt-and-axios-interceptor/server/src/app/modules/auth/auth.utils.js
+++
@@ -1,8 +0,0 @@
-import config from '../../../config/index.js';
-
-export const cookieOptions = {
-  httpOnly: true, // Protects the cookie from client-side scripts
-  secure: config.env === 'production', // Ensures cookie is sent over HTTPS in production
-  sameSite: config.env === 'production' ? 'none' : 'strict',
-  // maxAge: 60 * 60 * 1000, // 60 minutes expiration
-};
